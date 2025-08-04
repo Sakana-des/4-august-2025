36a1b52b00e1b3c97630f63e665665c6b8659900
Atha khoirun ayyasi <iyas123yas@gmail.com>
2025-08-04 13:19:56 +0700

    Add files via upload

--- AnimeSlaluDihati.java
+++ AnimeSlaluDihati.java
@@ -1,9 +1,14 @@
 public class AnimeSlaluDihati {
-
     public static void main(String[] args) {
+        //main method
         System.out.println("Atha khoirun ayyasi"); 
-         
-          
+           System.out.println("SMK TELKOM MALANG");
+            System.out.println("SMP Negeri 24 Malang");
+             System.out.println("Soni Riswanto");
+            System.out.println("Suci candrawati");
+           System.out.println("Pengusaha sukses");
+        System.out.println("Nonton Anime");
     
+        
     }
 }
