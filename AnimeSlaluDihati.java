f3335eec14e0ae91d00f38f7a7d7a1bf24254dff
Atha khoirun ayyasi <iyas123yas@gmail.com>
2025-08-04 12:02:12 +0700

    Add files via upload

--- AnimeSlaluDihati.java
+++ AnimeSlaluDihati.java
@@ -1,9 +1,8 @@
 public class AnimeSlaluDihati {
 
     public static void main(String[] args) {
-        System.out.println("Waifumu apa bos!"); 
-         System.out.println("Pakenanya, elainalah");
-          System.out.println("Mantap");
+        System.out.println("Atha khoirun ayyasi"); 
+         
           
     
     }
